d00a2aa7650f54ee1f18acea97b11082c8cebd23
MoreMar <103083468+MoreMarciani@users.noreply.github.com>
2023-06-14 23:31:14 -0300

    Add files via upload

--- tp1/sketch.js
+++ tp1/sketch.js
@@ -1,130 +1,244 @@
-let squares = [];
-let centerX, centerY;
-let isGrouped = false;
-let imagen1;
+// Variables de sonido
+
+let monitorear = false;
+
+//------VARIABLES A CALIBRAR-----//
+let AMP_MIN = 0.006; // umbral mínimo de amplitud. Señal que supera al ruido de fondo
+let AMP_MAX = 0.03; // umbral máximo de amplitud. 
+let FREC_MIN = 160;
+let FREC_MAX = 900;
+
+//-----ENTRADA DE AUDIO----
+let mic;
+let pitch;
+let audioContext; //motor de audio del navegador
+//-----GESTOR DE AMP Y PITCH----//
+let gestorPitch;
+let gestorAmp;
+//-----AMPLITUD----
+let amp; //variable donde cargo los valores de amplitud del sonido de entrada
+let haySonido = false; // vaiable buleana que de define el ESTADO
+let antesHabiaSonido = false; //memoria de la variable "haySonido". Guarda el valor de la variable en fotograma anterior
+let antesHabiaFrec = false;
+//----FRECUENCIA -----
+let frecuencia; //variable donde cargo los valores de frecuencia del sonido de entrada
+let frecuenciaAnterior; //memoria de la variable "frecuencia". Guarda el valor de la variable en fotograma anterior
+
+const model_url = 'https://cdn.jsdelivr.net/gh/ml5js/ml5-data-and-models/models/pitch-detection/crepe/';
+
+// Variables de imagen y estado
+let img1, img2, img3, img4, img5, img6, img8,img9, img10;
+let estado = "inicial";
+let marcaDeTiempo;
+let limiteInicial = 2000;
+let limiteMov = 2000;
+let vel;
+
+//FONDO
 let graphics;
-let trazos = [];
-let cantidad = 19;
+let fondos = [];
+let cantidad = 3;
 
-function preload(){
-    for( let i=0 ; i<cantidad ; i++){
-        let nombre = "data/trazo"+nf( i , 2 )+".png";
-        //console.log( nombre );
-        trazos[i] = loadImage( nombre );
-      }
 
-    imagen1 = loadImage('data/fig00.png');
+
+
+
+function preload() {
+  // Carga las imágenes
+  img1 = loadImage('data/fig00.png');
+  img2 = loadImage('data/fig01.png');
+  img3 = loadImage('data/fig02.png');
+  img4 = loadImage('data/fig03.png');
+  img5 = loadImage('data/fig04.png');
+  img6 = loadImage('data/fig05.png');
+  img7 = loadImage('data/fig06.png');
+  img8 = loadImage('data/fig07.png');
+  img9 = loadImage('data/fig08.png');
+  img10 = loadImage('data/fig09.png');
+
+  //fondo
+   for( let i=0 ; i<cantidad ; i++){
+    let nombre = "data/fondo"+nf( i , 2 )+".png";
+    //console.log( nombre );
+    fondos[i] = loadImage( nombre );
+  } 
 }
 
+function setup() {
+  createCanvas(displayWidth, displayHeight);
+ 
+  imageMode(CENTER);
+  //---CONFIG DE MIC Y AUDIO---//
+  audioContext = getAudioContext();
+  mic = new p5.AudioIn();
+  mic.start(startPitch);
+  userStartAudio(); // esto lo utilizo porque en algunos navigadores se cuelga el audio. Esto hace un reset del motor de audio (audio context)
 
-function setup(){
-createCanvas(windowWidth, windowHeight);
-background(120);
-  centerX = width/2;
- centerY = height/2;
- graphics = createGraphics(windowWidth,windowHeight);
-squares.push(createSquare(width / 4, height / 4));
-squares.push(createSquare(width * 3 / 4, height / 4));
-squares.push(createSquare(width / 4, height * 3 / 4));
-squares.push(createSquare(width * 3 / 4, height * 3 / 4));
-//squares.push(createSquare(width / 2, height/2+200));
-//squares.push(createSquare(width * 3 / 4, height * 3 / 4));
+  //--- LLAMO AL GESTOR DE AMP Y PITCH---//
+  gestorAmp = new GestorSenial(AMP_MIN, AMP_MAX);
+  gestorPitch = new GestorSenial(FREC_MIN, FREC_MAX)
 
+  //---DECLARO VARIABLES---//
+  antesHabiaSonido = false;
 }
-function createSquare(x, y) {
-    return {
-      x: x,
-      y: y,
-      size: 250,
-      rotation: 0,
-      rotationSpeed: random(0.01, 0.05),
-      maxSpeed: 0.1,
-      action: function () {
-        let distance = dist(mouseX, mouseY, this.x, this.y);
-  
-        // Aumenta el tamaño del cuadrado al mover el mouse
-        this.size = map(distance, 0, width, 250, 100);
-  
-        // Aumenta la velocidad de rotación del cuadrado al mover el mouse
-        this.rotationSpeed = map(distance, 0, width, 0, this.maxSpeed);
-      },
-    };
+
+function draw() {
+  background(255);
+  //Declaramos e inicializamos variables que ayuden al sonido
+  let vol = mic.getLevel();//cargo en vol, la apm del mic (señal cruda)
+  gestorAmp.actualizar(vol);
+  //empiezo a definir si hay sonido o no, cuando eso ocurre
+  haySonido = gestorAmp.filtrada > 0.1; //umbral de ruido, si hay sonido
+
+ 
+
+  let iniciarSonido = haySonido && !antesHabiaSonido; //EVENTO DEL INICIO DEL SONIDO
+  let finDelSonido = !haySonido && antesHabiaSonido; // EVENTO DEL FINAL DE UN SONIDO
+
+ 
+
+
+
+  //monitoreo de gestorPitch y gestorAmp
+  if(monitorear){
+    gestorAmp.dibujar(100,200);
+    gestorPitch.dibujar(100, 350);
   }
 
-  function draw() {
-    background(0,0,20);
-    // graphics
-    image(graphics,0,0);
-    //graphics.background(trazos[cual]);
-    //fondo
-    let cual = int(random(cantidad));
-    let x = random(width);
-    let y = random(height);
-    push()
-    imageMode(CENTER);
-    graphics.image(trazos[cual], x, y, 200,200);
-    pop()
-  
-    // Agrupa los cuadrados en el centro de la pantalla si el mouse está cerca del centro
-    let distanceToCenter = dist(mouseX, mouseY, centerX, centerY);
-    if (distanceToCenter < 50) {
-      isGrouped = true;
-    } else {
-      isGrouped = false;
-    }
-  
-    // Actualiza y dibuja los cuadrados
-    for (let i = 0; i < squares.length; i++) {
-      let square = squares[i];
-  
-      // Aplica la función correspondiente al cuadrado al mover el mouse
-      square.action();
-  
-      // Rota el cuadrado
-      square.rotation += square.rotationSpeed;
-  
-      // Si los cuadrados están agrupados, mueve cada cuadrado hacia el centro
-      if (isGrouped) {
-        square.x = lerp(square.x, centerX, 0.1);
-        square.y = lerp(square.y, centerY, 0.1);
-      }
-  
-      // Dibuja el cuadrado
+ antesHabiaSonido = haySonido;
+
+ let frec = map(gestorPitch.filtrada, 0, 1, FREC_MIN, FREC_MAX); //Mapeo para la frecuencia 
+ 
+
+
+//--- ACÁ VIENE LO DE LAS FIGURAS Y FONDO---//
+if(estado == "inicial"){ //ESTADO INICIO, LAS FIGURAS ESTATICAS
+  background(255);
+   image(fondos[1], width/2, height/2, width, height)
+ 
+    image(img1, frec+200, 300, 100, 100); //00 // DE IZQ A DER 
+    image(img2, frec+200, 100, 150, 150); // 01 // DE IZQ A DER
+    image(img3, 700, frec+100, 200, 200); //02 //DE ARRIBA A ABAJO
+    image(img4, 1100-frec, 100, 200, 200); //03 //DE DER A IZQ
+    image(img5, 980-frec, 300, 100,100); //04 // DE DER A IZQ
+    image(img6, frec+200, 450, 100,100); //05 // DE IZQ A DER
+    image(img7, 500, 500-frec, 150, 150); //06 // ABAJO HACIA ARRIBA
+    image(img8, 450, 90, frec+200,frec+200); //07 //SE AGRANDA Y ACHICA
+    image(img9, 850, 480, 300-frec,300-frec); //08 //SE AGRANDA Y ACHICA
+    image(img10, 1200-frec, 400, 250, 250); //09 //DE DER A IZQ
+    
+
+  if(iniciarSonido){
+    marcaDeTiempo = millis();
+    
+  }
+
+  if(finDelSonido){
+   // marcaDeTiempo = millis();
+    
+  }
+
+ let cambiarYa = millis();
+ if(cambiarYa > marcaDeTiempo + limiteInicial){
+  estado = "movimiento"
+  marcaDeTiempo = millis();
+ }
+
+}
+else if(estado == "movimiento"){//SE MUEVEN CON AGUDOS y GRAVES
+  if(haySonido){ 
+    //el fondo se reemplaza cuando el sonido esta
+   image(fondos[1], width/2, height/2, width, height);
+    //aca van las funciones de poder agrupar las img en el centro o cerca de ahi
+    //vel = frec;
+    image(img1, frec+200, 300, 100, 100); //00 // DE IZQ A DER 
+    image(img2, frec+200, 100, 150, 150); // 01 // DE IZQ A DER
+    image(img3, 700, frec+100, 200, 200); //02 //DE ARRIBA A ABAJO
+    image(img4, 1100-frec, 100, 200, 200); //03 //DE DER A IZQ
+    image(img5, 980-frec, 300, 100,100); //04 // DE DER A IZQ
+    image(img6, frec+200, 450, 100,100); //05 // DE IZQ A DER
+    image(img7, 500, 500-frec, 150, 150); //06 // ABAJO HACIA ARRIBA
+    image(img8, 450, 90, frec+200,frec+200); //07 //SE AGRANDA Y ACHICA
+    image(img9, 850, 480, 300-frec,300-frec); //08 //SE AGRANDA Y ACHICA
+    image(img10, 1200-frec, 400, 250, 250); //09 //DE DER A IZQ
+    
+  }
+  else if(!haySonido){
+    image(fondos[2], width/2, height/2, width, height)
+    vel -= 0;
+    image(img1, frec+200, 300, 100, 100); //00 // DE IZQ A DER 
+    image(img2, frec+200, 100, 150, 150); // 01 // DE IZQ A DER
+    image(img3, 700, frec+100, 200, 200); //02 //DE ARRIBA A ABAJO
+    image(img4, 1100-frec, 100, 200, 200); //03 //DE DER A IZQ
+    image(img5, 980-frec, 300, 100,100); //04 // DE DER A IZQ
+    image(img6, frec+200, 450, 100,100); //05 // DE IZQ A DER
+    image(img7, 500, 500-frec, 150, 150); //06 // ABAJO HACIA ARRIBA
+    image(img8, 450, 90, frec+200,frec+200); //07 //SE AGRANDA Y ACHICA
+    image(img9, 850, 480, 300-frec,300-frec); //08 //SE AGRANDA Y ACHICA
+    image(img10, 1200-frec, 400, 250, 250); //09 //DE DER A IZQ
+  }
+
+ /* if(finDelSonido){
+    marcaDeTiempo = millis();
+
+  }
+  let cambiarYa = millis();
+ if(cambiarYa > marcaDeTiempo + limiteMov){
+  estado = "desagrupar"
+  marcaDeTiempo = millis();
+ }
+
+}
+else if(estado == "desagrupar"){ // SE DESAGRUPAN Y SE MUEVEN A OTRO PUNTO CON GRAVES
+
+   /* if (haySonido) {
+      let angulo = radians(frameCount); 
       push();
-      translate(square.x, square.y);
-      rotate(square.rotation);
-      fill(0);
-     /* rectMode(CENTER);
-      rect(0, 0, square.size, square.size);*/
-      imageMode(CENTER);
-      image(imagen1,0, 0, square.size, square.size);
-      pop();
-    }
-  
-    // Verifica si el mouse está en la parte superior derecha de la pantalla
-    if (mouseX > width * 0.8 && mouseY < height * 0.2) {
-      // Restaura las posiciones originales de los cuadrados
-      for (let i = 0; i < squares.length; i++) {
-        squares[i].x = width / 4 + (i % 2) * (width / 2);
-        squares[i].y = height / 4 + floor(i / 2) * (height / 2);
-      }
-      // Dibuja el cuadrado
-    push();
-    translate(square.x, square.y);
-    rotate(square.rotation);
-    fill(0);
-    rectMode(CENTER);
-    rect(0, 0, square.size, square.size);
-    pop();
-  }
-
-  // Verifica si el mouse está en la parte superior derecha de la pantalla
-  if (mouseX > width * 0.8 && mouseY < height * 0.2) {
-    // Restaura las posiciones originales de los cuadrados
-    for (let i = 0; i < squares.length; i++) {
-      squares[i].x = width / 4 + (i % 2) * (width / 2);
-      squares[i].y = height / 4 + floor(i / 2) * (height / 2);
-    }
+      rotate(angulo);
+      image(img1, 0,0,100,100)
+      pop()
+      
+    }*/
 }
 
+//console.log(estado);
+
+
+  
+
+
+
+ 
+}
+
+
+
+//---------------------PITCH DETECTION-----------------------------------------------
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+  }
+  //--------------------------------------------------------------------
+  function startPitch() {
+    pitch = ml5.pitchDetection(model_url, audioContext , mic.stream, modelLoaded);
+  }
+  //--------------------------------------------------------------------
+  function modelLoaded() {
+  //select('#status').html('Model Loaded');
+  getPitch();
+  //console.log( "entro aca !" );
+  
+  }
+  //--------------------------------------------------------------------
+  function getPitch() {
+    pitch.getPitch(function(err, frequency) {
+    if (frequency) {    	
+    //  let midiNum = freqToMidi(frequency);
+      //console.log( midiNum );
+  
+      gestorPitch.actualizar( frequency );
+     // console.log(frequency);
+  
+    }
+    getPitch();
+  })
   }
